cde91e9078dea7147b1a7542bba3814a4ca1e3fd
lam7 <kingwork6500@gmail.com>
2018-10-19 19:06:57 +0900

    棒グラフの表示がおかしいのを修正

--- DaihugouBattle/Possession/Deck/BarGraphView.swift
+++ DaihugouBattle/Possession/Deck/BarGraphView.swift
@@ -10,54 +10,27 @@ import Foundation
 import UIKit
 import SnapKit
 
-@IBDesignable class DeckIndexBarGraph: BarGraphView{
-    var deckIndexMax: Int?
-    var indexCounts: [Int : Int] = [:]{
-        didSet{
-            let indexCounts = self.indexCounts.sorted(by: { $0.key < $1.key })
-            let max = deckIndexMax ?? indexCounts.map({$0.value}).max() ?? 1
-            self.barRatings = indexCounts.map({ $0.value.cf / max.cf })
-            barViews.enumerated().forEach{ i, v in
-                v.barLayer.strokeColor = UIColor.green.cgColor
-                v.barLayer.borderWidth = 1.0
-                v.barLayer.borderColor = UIColor.white.cgColor
-                v.topLabel.text = indexCounts[i].value.description
-                v.bottomLabel.text = indexCounts[i].key.description
-            }
-        }
-    }
-    
-    func indexCounts(from deck: Deck){
-        indexCounts(from: deck.cards)
-    }
-    
-    func indexCounts(from cards: [Card]){
-        var indexCounts: [Int : Int] = [:]
-        cards.map({ $0.index }).forEach({
-            if indexCounts[$0] == nil{ indexCounts[$0] = 1}
-            else{ indexCounts[$0]! += 1}
-        })
-        self.indexCounts = indexCounts
-    }
-}
-@IBDesignable class BarGraphView: UIView{
+class BarGraphView: UIStackView{
     var barViews: [BarGraphBaseView] = []
-    var spacing: CGFloat = 0.0
+    
     var barRatings: [CGFloat] = []{
         didSet{
+            setNeedsDisplay()
+            layoutIfNeeded()
             if oldValue.count != barRatings.count{
                 barViews.forEach{ $0.removeFromSuperview() }
                 barViews = []
-                var w = bounds.width
-                w -= (barRatings.count.cf - 1) * spacing
-                w /= barRatings.count.cf
-                for (i, rate) in barRatings.enumerated(){
-                    let x = i.cf * (w + spacing)
-                    let f = CGRect(x: x, y: 0, width: w, height: bounds.height)
-                    let v = BarGraphBaseView(frame: f)
-                    addSubview(v)
+                let w = self.bounds.width / barRatings.count.cf
+                for rate in barRatings{
+                    let v = BarGraphBaseView()
+                    addArrangedSubview(v)
+                    v.snp.makeConstraints{ make in
+                        make.height.equalTo(self)
+                        make.width.equalTo(w)
+                        make.centerY.equalTo(self)
+                    }
+                    self.barViews.append(v)
                     v.barLayer.update(to: rate)
-                    barViews.append(v)
                 }
             }else{
                 barViews.enumerated().forEach{ i, v in
@@ -72,96 +45,45 @@ import SnapKit
         setUp()
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setUp()
     }
     
+    
     private func setUp(){
-        
+        alignment = .top
+        axis = .horizontal
+        distribution = .equalSpacing
     }
 }
 
-class BarGraphBaseView: UIView{
-    weak var topLabel: UILabel!
-    weak var barView: BarGraphBaseLayerView!
-    var barLayer: BarGraphBaseLayer!{
-        return barView.barLayer
+class BarGraphBaseView: UINibView{
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var barView: BarGraphBaseLayerView!
+    var barLayer: BarGraphBaseLayer{
+        return barView.layer as! BarGraphBaseLayer
     }
-    weak var bottomLabel: UILabel!
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUp()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setUp()
-    }
-    
-    private func setUp(){
-        let w = bounds.width
-        let h = bounds.height * 0.15
-        let topLabel = UILabel(frame: CGRect(x: 0, y: 0, width: w, height: h))
-        topLabel.textAlignment = .center
-        topLabel.adjustsFontSizeToFitWidth = true
-        addSubview(topLabel)
-        self.topLabel = topLabel
-        
-        let bottomLabel = UILabel(frame: CGRect(x: 0, y: bounds.height - h, width: w, height: h))
-        bottomLabel.textAlignment = .center
-        bottomLabel.adjustsFontSizeToFitWidth = true
-        addSubview(bottomLabel)
-        self.bottomLabel = bottomLabel
-        
-        let barView = BarGraphBaseLayerView(frame: CGRect(x: 0, y: h, width: w, height: bounds.height - h * 2))
-        addSubview(barView)
-        self.barView = barView
-    }
+    @IBOutlet weak var bottomLabel: UILabel!
 }
 
 class BarGraphBaseLayerView: UIView{
-    var barLayer: BarGraphBaseLayer!
-    override var frame: CGRect{
-        didSet{
-            if barLayer != nil{
-                barLayer.frame = bounds
-                dump(barLayer.frame)
-            }
-        }
-    }
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUp()
+    override class var layerClass : AnyClass {
+        return BarGraphBaseLayer.self
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setUp()
-    }
-    
-    private func setUp(){
-        let l = BarGraphBaseLayer()
-        layer.addSublayer(l)
-        self.barLayer = l
-        l.frame = bounds
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: bounds.width / 2, y: bounds.height))
+        path.addLine(to: CGPoint(x: bounds.width / 2, y: 0))
+        path.lineWidth = frame.width
+        (layer as! BarGraphBaseLayer).lineWidth = frame.width
+        (layer as! BarGraphBaseLayer).path = path.cgPath
     }
 }
 class BarGraphBaseLayer: CAShapeLayer{
-    override var frame: CGRect{
-        didSet{
-            let path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.width / 2, y: bounds.height))
-            path.addLine(to: CGPoint(x: bounds.width / 2, y: 0))
-            path.lineWidth = frame.width
-            self.lineWidth = frame.width
-            self.path = path.cgPath
-        }
-    }
-    
     func update(to ratio: CGFloat, duration: CFTimeInterval = 0.25, completion: (() -> ())? = nil) {
-//        dump(frame)
         CATransaction.begin()
         strokeEnd = ratio
         CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
---
+++ DaihugouBattle/Possession/Deck/DeckIndexBarGraph.swift
@@ -0,0 +1,45 @@
+//
+//  DeckIndexBarGraph.swift
+//  DaihugouBattle
+//
+//  Created by main on 2018/10/19.
+//  Copyright © 2018 Main. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DeckIndexBarGraph: BarGraphView{
+    var deckIndexMax: Int?
+    var indexCounts: [Int : Int] = [:]{
+        didSet{
+            if self.indexCounts.isEmpty { return }
+            let indexCounts = self.indexCounts.sorted(by: { $0.key < $1.key })
+            let max = deckIndexMax ?? indexCounts.map({$0.value}).max() ?? 1
+            self.barRatings = indexCounts.map({ $0.value.cf / max.cf })
+            barViews.enumerated().forEach{ i, v in
+                v.barLayer.strokeColor = UIColor.green.cgColor
+                v.barLayer.borderWidth = 1.0
+                v.barLayer.borderColor = UIColor.white.cgColor
+                v.topLabel.text = indexCounts[i].value.description
+                v.bottomLabel.text = indexCounts[i].key.description
+            }
+        }
+    }
+    
+    func indexCounts(from deck: Deck){
+        indexCounts(from: deck.cards)
+    }
+    
+    func indexCounts(from cards: [Card]){
+        var indexCounts: [Int : Int] = [:]
+        for i in StandartCardIndexRange{
+            indexCounts[i] = 0
+        }
+        cards.map({ $0.index }).forEach({
+            if indexCounts[$0] == nil{ indexCounts[$0] = 1}
+            else{ indexCounts[$0]! += 1}
+        })
+        self.indexCounts = indexCounts
+    }
+}
