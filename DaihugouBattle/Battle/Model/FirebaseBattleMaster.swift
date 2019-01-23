5008bd67b5302cffc76cfc154faeb2ad2b269316
lam7 <kingwork6500@gmail.com>
2019-01-23 17:54:11 +0900

    update: BattleのFirebase周りを変更

--- DaihugouBattle/Battle/Model/FirebaseBattleMaster.swift
+++ DaihugouBattle/Battle/Model/FirebaseBattleMaster.swift
@@ -10,189 +10,204 @@ import Foundation
 
 
 
-//class FirebaseBattleMaster: BattleMaster{
-//    var battleField: BattleField!
-//    
-//    var delegate: BattleFieldDelegate?
-//    
-//    var spotAtkRate: Float!
-//    private var ownerDeck: DeckBattle
-//    private let NumberOfInitialHands = 5
-//    private let NumberOfRevolution = 4
-//    
-//    func gameStart(_ completion: @escaping (Error?) -> ()){
-//        ownerDeck.shuffle()
-//        drawOwnerCards(NumberOfInitialHands)
-//        drawEnemyCards(NumberOfInitialHands)
-//        
-//        if isOwnerTurn{
-//            ownerTurn()
-//        }else{
-//            enemyTurn()
-//        }
-//        completion(nil)
-//    }
-//    
-//    private func drawOwnerCards(_ amount: Int = 1){
-//        let cards = ownerDeck.drawCards(amount)
-//        if !cards.filter({ $0 == nil }).isEmpty{
-//            battleField.owner.attacked(9999)
-//        }else{
-//            battleField.owner.drawCards(cards.compactMap{ $0 })
-//        }
-//    }
-//    
-//    private func drawEnemyCards(_ amount: Int = 1){
-//        let cards = (0 ..< amount).map{ _ in CardBattle(card: cardNoData) }
-//        battleField.enemy.drawCards(cards.compactMap{ $0 })
-//    }
-//    
-//    private func putDown(_ cards: [Card], player: Player) -> Error?{
-//        if cards.isEmpty{ return nil }
-//        
-//        var hand = player.hand
-//        //ハンドに全てのカードがあるかチェック
-//        guard cards.filter({ hand.contains($0) }).count == cards.count else{
-//            return Errors.Battle.notExistCardsInHand
-//        }
-//        
-//        for card in cards{
-//            let index = hand.index(of: card)!
-//            hand.remove(at: index)
-//        }
-//        
-//        player.putDown(cards)
-//        player.activateSkill(cards, activateType: .fanfare)
-//        battleField.table.changeSpotStatus(by: cards)
-//        
-//        player.changeAtkRate(inc: 0.1)
-//        if player.id == battleField.owner.id{
-//            print("ownerCards")
-//            player.changeOrignalAtk(to: ownerOriginalAtk)
-//            player.changeAtk(to: ownerAtk)
-//        }else{
-//            print("enemyCards")
-//            player.changeOrignalAtk(to: enemyOriginalAtk)
-//            player.changeAtk(to: enemyAtk)
-//        }
-//        
-//        if cards.count >= NumberOfRevolution{
-//            let current = battleField.table.currentCardStrength
-//            let next: CardStrength = current == .normal ? .revolution : .normal
-//            battleField.table.changeCurrentCardStrength(next)
-//        }
-//        return nil
-//    }
-//    
-//    func ownerPutDown(_ cards: [Card], completion: @escaping (Error?) -> ()) {
-//        guard isOwnerTurn else{
-//            completion(Errors.Battle.isNotOwnerTurn)
-//            return
-//        }
-//        guard battleField.spot.canPutDown(cards) else{
-//            completion(Errors.Battle.cannotPutDown)
-//            return
-//        }
-//        
-//        if let error = putDown(cards, player: battleField.owner){
-//            completion(error)
-//            return
-//        }
-//        
-//        ownerTurnEnd()
-//        completion(nil)
-//    }
-//    
-//    func ownerPass(_ completion: @escaping (Error?) -> ()) {
-//        guard isOwnerTurn else{
-//            completion(Errors.Battle.isNotOwnerTurn)
-//            return
-//        }
-//        
-//        attack(false)
-//        battleField.table.changeSpotStatus(by: [])
-//        ownerTurnEnd()
-//        
-//        completion(nil)
-//    }
-//    
-//    private func ownerTurnEnd(){
-//        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .ownerTurnEnd)
-//        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .enemyTurnEnd)
-//        
-//        delegate?.endOwnerTurn()
-//        isOwnerTurn = !isOwnerTurn
-//        enemyTurn()
-//    }
-//    
-//    private func ownerTurn(){
-//        delegate?.startOwnerTurn()
-//        drawOwnerCards(1)
-//        
-//        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .ownerTurnStart)
-//        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .enemyTurnStart)
-//    }
-//    private func startEnemyTurn(){
-//        delegate?.startEnemyTurn()
-//        
-//        battleField.owner.drawCards([cardNoData])
-//        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .ownerTurnStart)
-//        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .enemyTurnStart)
-//    }
-//    
-//    private func enemyPutDown(_ cards: [Card]){
-//        delegate?.startEnemyTurn()
-//        
-//        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .ownerTurnStart)
-//        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .enemyTurnStart)
-//        
-//        if cards.isEmpty || putDown(cards, player: battleField.enemy) != nil{
-//            attack(true)
-//            battleField.table.changeSpotStatus(by: [])
-//        }
-//        
-//        enemyTurnEnd()
-//    }
-//    
-//    
-//    private func enemyTurnEnd(){
-//        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .ownerTurnEnd)
-//        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .enemyTurnEnd)
-//        
-//        delegate?.endEnemyTurn()
-//        isOwnerTurn = !isOwnerTurn
-//        ownerTurn()
-//    }
-//    
-//    func attack(_ isFirstOwner: Bool){
-//        let p1 = isFirstOwner ? battleField.owner : battleField.enemy
-//        let p2 = !isFirstOwner ? battleField.owner : battleField.enemy
-//        let p1Cards = isFirstOwner ? battleField.spot.ownerCards : battleField.spot.enemyCards
-//        let p2Cards = !isFirstOwner ? battleField.spot.ownerCards : battleField.spot.enemyCards
-//        
-//        
-//        p1.activateSkill(p1Cards, activateType: .beforeAttack)
-//        let p1Atk = isFirstOwner ? ownerAtk : enemyAtk
-//        p1.attack(p1Atk)
-//        
-//        p2.activateSkill(p2Cards, activateType: .beforeAttack)
-//        let p2Atk = !isFirstOwner ? ownerAtk : enemyAtk
-//        p2.attack(p2Atk)
-//        
-//        p1.passingCametery(p1Cards)
-//        p1.activateSkill(p1Cards, activateType: .lastword)
-//        p2.passingCametery(p2Cards)
-//        p2.activateSkill(p2Cards, activateType: .lastword)
-//        
-//        p1.changeOrignalAtk(to: 0)
-//        p2.changeOrignalAtk(to: 0)
-//        p1.changeAtkRate(to: 1.0)
-//        p2.changeAtkRate(to: 1.0)
-//        p1.changeAtk(to: 0)
-//        p2.changeAtk(to: 0)
-//        
-//        //テーブルにあるカードを流す
-//        battleField.spot.removeAll()
-//    }
-//}
-//
+class FirebaseBattleMaster: BattleMaster{
+    var battleField: BattleField!
+    var battleRoom: FirebaseBattleRoom
+    var delegate: BattleFieldDelegate?
+    
+    var spotAtkRate: Float! = 1.0
+    var isOwnerTurn: Bool = false
+    private var ownerDeck: DeckBattle
+    private let NumberOfInitialHands = 5
+    private let NumberOfRevolution = 4
+    
+    
+    init(){
+        battleRoom = FirebaseBattleRoom()
+    }
+    
+    func matching(_ completion: @escaping (_ error: Error?) -> ()){
+        battleRoom.setUp(completion)
+    }
+    
+    func gameStart(_ completion: @escaping (Error?) -> ()){
+        ownerDeck.shuffle()
+        drawOwnerCards(NumberOfInitialHands)
+        drawEnemyCards(NumberOfInitialHands)
+        
+        if isOwnerTurn{
+            ownerTurn()
+        }else{
+            enemyTurn()
+        }
+        completion(nil)
+    }
+    
+    private func drawOwnerCards(_ amount: Int = 1){
+        let cards = ownerDeck.drawCards(amount)
+        if !cards.filter({ $0 == nil }).isEmpty{
+            battleField.owner.attacked(9999)
+        }else{
+            battleField.owner.drawCards(cards.compactMap{ $0 })
+        }
+        
+    }
+    
+    private func drawEnemyCards(_ amount: Int = 1){
+        let cards = (0 ..< amount).map{ _ in CardBattle(card: cardNoData) }
+        battleField.enemy.drawCards(cards.compactMap{ $0 })
+    }
+    
+    private func putDown(_ cards: [Card], player: Player) -> Error?{
+        if cards.isEmpty{ return nil }
+        
+        var hand = player.hand
+        //ハンドに全てのカードがあるかチェック
+        guard cards.filter({ hand.contains($0) }).count == cards.count else{
+            return Errors.Battle.notExistCardsInHand
+        }
+        
+        for card in cards{
+            let index = hand.index(of: card)!
+            hand.remove(at: index)
+        }
+        
+        player.putDown(cards)
+        player.activateSkill(cards, activateType: .fanfare)
+        battleField.table.changeSpotStatus(by: cards)
+        
+        player.changeAtkRate(inc: 0.1)
+        if player.id == battleField.owner.id{
+            print("ownerCards")
+            player.changeOrignalAtk(to: ownerOriginalAtk)
+            player.changeAtk(to: ownerAtk)
+        }else{
+            print("enemyCards")
+            player.changeOrignalAtk(to: enemyOriginalAtk)
+            player.changeAtk(to: enemyAtk)
+        }
+        
+        if cards.count >= NumberOfRevolution{
+            let current = battleField.table.currentCardStrength
+            let next: CardStrength = current == .normal ? .revolution : .normal
+            battleField.table.changeCurrentCardStrength(next)
+        }
+        return nil
+    }
+    
+    func ownerPutDown(_ cards: [Card], completion: @escaping (Error?) -> ()) {
+        guard isOwnerTurn else{
+            completion(Errors.Battle.isNotOwnerTurn)
+            return
+        }
+        guard battleField.spot.canPutDown(cards) else{
+            completion(Errors.Battle.cannotPutDown)
+            return
+        }
+        
+        if let error = putDown(cards, player: battleField.owner){
+            completion(error)
+            return
+        }
+        
+        ownerTurnEnd()
+        completion(nil)
+    }
+    
+    func ownerPass(_ completion: @escaping (Error?) -> ()) {
+        guard isOwnerTurn else{
+            completion(Errors.Battle.isNotOwnerTurn)
+            return
+        }
+        
+        attack(false)
+        battleField.table.changeSpotStatus(by: [])
+        ownerTurnEnd()
+        
+        completion(nil)
+    }
+    
+    private func ownerTurnEnd(){
+        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .ownerTurnEnd)
+        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .enemyTurnEnd)
+        
+        delegate?.endOwnerTurn()
+        isOwnerTurn = !isOwnerTurn
+        enemyTurn()
+    }
+    
+    private func ownerTurn(){
+        delegate?.startOwnerTurn()
+        drawOwnerCards(1)
+        
+        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .ownerTurnStart)
+        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .enemyTurnStart)
+    }
+    
+    private func startEnemyTurn(){
+        delegate?.startEnemyTurn()
+        
+        battleField.owner.drawCards([cardNoData])
+        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .ownerTurnStart)
+        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .enemyTurnStart)
+    }
+    
+    private func enemyPutDown(_ cards: [Card]){
+        delegate?.startEnemyTurn()
+        
+        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .ownerTurnStart)
+        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .enemyTurnStart)
+        
+        if cards.isEmpty || putDown(cards, player: battleField.enemy) != nil{
+            attack(true)
+            battleField.table.changeSpotStatus(by: [])
+        }
+        
+        enemyTurnEnd()
+    }
+    
+    private func enemyTurnEnd(){
+        battleField.enemy.activateSkill(battleField.spot.enemyCards, activateType: .ownerTurnEnd)
+        battleField.owner.activateSkill(battleField.spot.ownerCards, activateType: .enemyTurnEnd)
+        
+        delegate?.endEnemyTurn()
+        isOwnerTurn = !isOwnerTurn
+        ownerTurn()
+    }
+    
+    func attack(_ isFirstOwner: Bool){
+        let p1 = isFirstOwner ? battleField.owner : battleField.enemy
+        let p2 = !isFirstOwner ? battleField.owner : battleField.enemy
+        let p1Cards = isFirstOwner ? battleField.spot.ownerCards : battleField.spot.enemyCards
+        let p2Cards = !isFirstOwner ? battleField.spot.ownerCards : battleField.spot.enemyCards
+        
+        
+        p1.activateSkill(p1Cards, activateType: .beforeAttack)
+        let p1Atk = isFirstOwner ? ownerAtk : enemyAtk
+        p1.attack(p1Atk)
+        
+        p2.activateSkill(p2Cards, activateType: .beforeAttack)
+        let p2Atk = !isFirstOwner ? ownerAtk : enemyAtk
+        p2.attack(p2Atk)
+        
+        p1.passingCametery(p1Cards)
+        p1.activateSkill(p1Cards, activateType: .lastword)
+        p2.passingCametery(p2Cards)
+        p2.activateSkill(p2Cards, activateType: .lastword)
+        
+        p1.changeOrignalAtk(to: 0)
+        p2.changeOrignalAtk(to: 0)
+        p1.changeAtkRate(to: 1.0)
+        p2.changeAtkRate(to: 1.0)
+        p1.changeAtk(to: 0)
+        p2.changeAtk(to: 0)
+        
+        //テーブルにあるカードを流す
+        battleField.spot.removeAll()
+    }
+    
+    private func enemyTurn(){
+        
+    }
+}
+
--- DaihugouBattle/Battle/Model/FirebaseBattleRoom.swift
+++ DaihugouBattle/Battle/Model/FirebaseBattleRoom.swift
@@ -21,7 +21,9 @@ class FirebaseBattleRoom{
     private let rootRef = Database.database().reference()
     private let userRef  = Database.database().reference().child("users")
     private var roomId: String!
-    private var isBattling: Bool = false
+    private(set) var isBattling: Bool = false
+    private(set) var isYourTurn: Bool = false
+    
     
     func setUp(_ completion: @escaping (_ error: Error?) -> ()){
         login{ error in
@@ -33,14 +35,14 @@ class FirebaseBattleRoom{
         }
     }
     
-    func login(_ completion: @escaping (_ error: Error?) -> ()){
+    private func login(_ completion: @escaping (_ error: Error?) -> ()){
         Auth.auth().signInAnonymously{
             user, error in
             completion(error)
         }
     }
     
-    func getRoom(){
+    private func getRoom(){
         //一回だけwaitingFlgが１のユーザを取得
         userRef.queryOrdered(byChild: "isWaiting").queryEqual(toValue: "1").observeSingleEvent(of: .value, with: { (snapshot) in
             if let value = snapshot.value as? NSDictionary{
@@ -63,12 +65,14 @@ class FirebaseBattleRoom{
     
     private func createNewRoom(){
         self.roomId = UUID().uuidString
+        let isYourTurn = Bool.random()
+        self.isYourTurn = isYourTurn
         let user: [String : String] = [
             "masterObjectId" : UserLogin.objectIdUserInfo,
             "masterName" : UserInfo.shared.nameValue,
             "isMatching" : "0",
             "isWaiting" : "1",
-            "isMasterTurn" : (0...1).randomElement()!.description
+            "isMasterTurn" : isYourTurn ? "1" : "0"
         ]
         userRef.child(roomId).setValue(user)
     }
@@ -85,7 +89,6 @@ class FirebaseBattleRoom{
     
     private func getMessages(){
         isBattling = true
-        
         //【非同期】子要素が増えるたびにmessageに値を追加する。
         
         userRef.child(roomId).child("battleLogs").queryLimited(toLast: 1000).observe(DataEventType.childAdded, with: { (snapshot) in
@@ -102,6 +105,7 @@ class FirebaseBattleRoom{
     private func mathingRoom(_ value: Dictionary<AnyHashable, Any>){
         //対戦を始める相手を決定する
         for (key,val) in value {
+            if key == ""
             //このへんでレート補正
             roomId = key as? String
             break
@@ -111,23 +115,56 @@ class FirebaseBattleRoom{
     
     
     private func decodeMessage(_ dictionary: NSDictionary){
-        guard let text = (dictionary["text"] as? String) else{ return }
-        let texts = text.components(separatedBy: ",")
-        let act = texts[0]
-        let value = texts[1]
+        guard let id = dictionary["id"] as? String,
+            id == UserLogin.objectIdUserInfo,
+            let act = dictionary["act"] as? String,
+            let value = dictionary["value"] as? String else{ return }
         switch act{
         case "putDown":
-            let cards = (value as! [Int]).map{ CardList.get(id: $0)! }
-            delegate?.enemyPutDown(cards)
+            guard let array = value as? [Int] else{
+                print("--------------------------")
+                print("FirebaseBattleRoom Error")
+                print("value is mistake")
+                print(value)
+                return
+            }
+            
+            let cards = array.map({ CardList.get(id: $0) })
+            guard !cards.contains(nil) else{
+                print("--------------------------")
+                print("FirebaseBattleRoom Error")
+                print("value is mistake")
+                print(array)
+                return
+            }
+            delegate?.enemyPutDown(cards.compactMap({ $0 }))
         case "pass":
             delegate?.enemyPass()
         default:
             print("--------------------------")
             print("FirebaseBattleRoom Error")
-            print("act value is mistake")
-            print(text)
+            print("act is mistake")
+            print(act)
             
         }
+    }
+    
+    func encodeMessagePutDown(_ cards: [Card]){
+        let post: [String : Any] = [
+            "id" : UserLogin.objectIdUserInfo,
+            "act" : "putDown",
+            "value" : cards.map({$0.id})
+        ]
+        userRef.child(roomId).child("battleLogs").setValue(post)
+    }
+    
+    func encodeMessagePass(_ cards: [Card]){
+        let post: [String : Any] = [
+            "id" : UserLogin.objectIdUserInfo,
+            "act" : "pass",
+            "value" : ""
+        ]
+        userRef.child(roomId).child("battleLogs").setValue(post)
     }
 }
 
